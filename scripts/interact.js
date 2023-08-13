82d9825354124931ed93165691d35f3ad56a6220
Hoanh An <hoanhan101@gmail.com>
2023-08-12 22:32:59 -0700

    Add some analytics

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,16 +1,52 @@
+const fs = require("fs");
+
 async function main() {
     // GET CONTRACT
-    const contractCodeName = "LeetBrian";
-    const contractAddress = "0x24a644d24d7112f251644beb5cfaefeb362967f8";
-    const LeetContract = await ethers.getContractFactory(contractCodeName);
-    const leetContract = LeetContract.attach(contractAddress);
-    console.log(await leetContract.name());
-    console.log(await leetContract.totalSupply());
+    // const contractCodeName = "LeetBrian";
+    // const contractAddress = "0xfe558d36edc45d9e852f7dbc6f1f8fcd1a4a6611";
+    // const LeetContract = await ethers.getContractFactory(contractCodeName);
+    // const leetContract = LeetContract.attach(contractAddress);
+    // console.log(await leetContract.name());
+
+    // const supply = await leetContract.totalSupply();
+    // console.log(await leetContract.totalSupply());
 
     // TOKENURI
-    const tokenURI = await leetContract.tokenURI(0);
-    const payload = JSON.parse(tokenURI.split("data:application/json,")[1]);
-    console.log(payload);
+    // let results = [];
+    // for (let i = 0; i < supply; i++) {
+    //     const tokenURI = await leetContract.tokenURI(i);
+    //     const payload = JSON.parse(tokenURI.split("data:application/json,")[1]);
+    //     console.log(payload);
+    //     results.push(payload);
+    // }
+    // fs.writeFileSync("./assets/test.json", JSON.stringify(results));
+
+    // ANALYTICS
+    let traitsDistribution = {};
+    let traitsCount = {};
+    let raw = fs.readFileSync("./assets/test.json");
+    let tokens = JSON.parse(raw);
+    for (let i = 0; i < tokens.length; i++) {
+        const count = tokens[i].attributes.length;
+        if (!(count in traitsCount)) {
+            traitsCount[count] = 0;
+        }
+        for (let j = 0; j < tokens[i].attributes.length; j++) {
+            const traitType = tokens[i].attributes[j].trait_type;
+            const traitValue = tokens[i].attributes[j].value;
+            if (!(traitType in traitsDistribution)) {
+                traitsDistribution[traitType] = {};
+            }
+
+            if (!(traitValue in traitsDistribution[traitType])) {
+                traitsDistribution[traitType][traitValue] = 0;
+            }
+            traitsDistribution[traitType][traitValue] += 1;
+        }
+        traitsCount[count] += 1;
+    }
+    console.log(traitsDistribution);
+    console.log(traitsCount);
 
     // MINT
     // const rarities = [
